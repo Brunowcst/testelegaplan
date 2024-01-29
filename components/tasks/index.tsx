1948f00208035768a79b60b2b56fcaa793fa343b
Brunowcst <brunoporfirio022@gmail.com>
2024-01-29 20:56:15 -0300

    update task list

--- components/tasks/index.tsx
+++ components/tasks/index.tsx
@@ -1,28 +1,40 @@
 'use client'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import sytles from './styles.module.css'
 import CardTasks from '../cardtaks'
 import Modal from '../modal'
-import { FormTask } from '../formModal'
+
+interface Task {
+  id: number;
+  title: string;
+}
 
 export default function TasksContainer() {
   const [isOpen, setIsOpen] = useState(false)
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   const handleCloseModal = () => {
     setIsOpen(false)
   }
+  
+  useEffect(() => {
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+      setTasks(JSON.parse(storedTasks));
+    }
+  }, [tasks]);
 
   return (
     <div className={sytles.mainContainer}>
         <div className={sytles.mainContent}>
           <p className={sytles.tasktoday}>Suas tarefas de hoje</p>
-          <CardTasks/>
+          {tasks.map((task) => (
+          <CardTasks id={task.id} key={task.id} title={task.title} />
+          ))}
             <p className={sytles.tasktoday}>Tarefas finalizadas</p>
-            <CardTasks/>
         </div>  
-
         <button className={sytles.button} onClick={() => setIsOpen(true)}>Adicionar nova tarefa</button>
-        <Modal textButton='Adicionar' typeModal={1} isOpen={isOpen} onClose={handleCloseModal} content={<FormTask/>}/>
+        <Modal textButton='Adicionar' typeModal={1} isOpen={isOpen} onClose={handleCloseModal} />
     </div>
   )
 }
