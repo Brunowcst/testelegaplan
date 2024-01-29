b221a523aaec272cc670cacd92bed64bb1ea8613
Brunowcst <brunoporfirio022@gmail.com>
2024-01-29 20:56:03 -0300

    update modal

--- components/modal/index.tsx
+++ components/modal/index.tsx
@@ -1,27 +1,70 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.css';
 
 export interface ModalProps {
   isOpen: boolean;
-  content?: React.ReactNode;
   onClose: () => void;
   textButton: string;
   typeModal : number;
 }
 
-const Modal: React.FC<ModalProps> = ({ isOpen, onClose, content, textButton, typeModal }) => {
+export interface Task {
+  id: number;
+  title: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ isOpen, onClose, textButton, typeModal }) => {
   if (!isOpen) return null;
+
+  const [taskTitle, setTaskTitle] = React.useState<string>('');
+  const [taskList, setTaskList] = React.useState<Task[]>([]);
+
+  const handleAddTask = () => {
+    if (taskTitle.trim() !== '') {
+      const newTask: Task = { id: Date.now(), title: taskTitle };
+
+      const storedTasks = localStorage.getItem('tasks');
+      const currentTasks = storedTasks ? JSON.parse(storedTasks) : [];
+
+      const updatedTaskList = [...currentTasks, newTask];
+
+      setTaskList(updatedTaskList);
+      localStorage.setItem('tasks', JSON.stringify(updatedTaskList));
+
+      setTaskTitle('');
+      onClose();
+    }
+  };
 
   return (
     <div className={styles.modalOverlay} onClick={onClose}>
       <div className={styles.modalContent} onClick={(e) => e.stopPropagation()}>
-        {content}
+        {typeModal === 1 ? 
+          <div className={styles.mainContent}>
+            <h4>Nova Tarefa</h4>
+            <div className={styles.inputContainer}>
+              <span>Titulo</span>
+              <input 
+                type="text" 
+                placeholder='Digite'
+                value={taskTitle}
+                onChange={(e) => setTaskTitle(e.target.value)}
+              />
+            </div>
+          </div>  
+          : (
+            <div className={styles.mainContent}>
+              <h4>Deletar Tarefa</h4>
+              <p>Tem certeza que você deseja deletar essa tarefa?</p>
+            </div>
+          )
+        }
         <div className={styles.buttonContainer}>
           <button className={styles.closeButton} onClick={onClose}>
             Cancelar
           </button>
-          <button className={typeModal === 1 ? styles.addButton : styles.deleteButton}>
+          <button className={typeModal === 1 ? styles.addButton : styles.deleteButton} onClick={handleAddTask}>
             {textButton}
           </button>
         </div>
