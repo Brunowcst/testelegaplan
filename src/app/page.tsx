bdf0b35e9677e2745f7cc94e8409006c9d17b26b
Brunowcst <brunoporfirio022@gmail.com>
2024-01-29 19:46:34 -0300

    update home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 import Header from "../../components/header";
+import TasksContainer from "../../components/tasks";
 
 export default function Home() {
   return (
-    <main >
+    <main className={styles.mainContent}>
      <Header/>
+     <TasksContainer/>
     </main>
   );
 }
