87a3dad19d10686163d7e82d0f85f651f1aa88ac
Brunowcst <brunoporfirio022@gmail.com>
2024-01-30 00:18:23 -0300

    update file name

---
+++ components/cardTasks/index.tsx
@@ -0,0 +1,61 @@
+'use client'
+import React, { useState } from 'react'
+import styles from './styles.module.css'
+import TrashIcon from '../../assets/trash.svg'
+import Image from 'next/image'
+import Modal from '../modal'
+
+interface Task {
+    id: number;
+    title: string;
+    checked: boolean
+}
+
+interface CardTasksProps extends Task {
+    onCheckboxChange: (taskId: number) => void;
+    onDeleteTask: (taskId: number) => void;
+}
+
+export default function CardTasks({id, title, onCheckboxChange, checked, onDeleteTask}: CardTasksProps) {
+    const [isOpen, setIsOpen] = useState(false)
+
+    const handleCloseModal = () => {
+      setIsOpen(false)
+    }
+
+    const handleCheckboxChange = () => {
+        onCheckboxChange(id);
+    };
+
+    const handleDeleteButtonClick = () => {
+        onDeleteTask && onDeleteTask(id);
+        handleCloseModal();
+    };
+
+  return (
+    <>
+        <div className={styles.cardContainer}>
+            <div className={styles.checkbox}>
+                <input
+                    onChange={handleCheckboxChange}
+                    checked={checked}
+                    type="checkbox"
+                    id={`chk${id}`}
+                    className={styles.checkbox}
+                    name={`chk${id}`}
+                />
+                <label htmlFor={`chk${id}`} className={`${styles.taskTitle} ${checked ? styles.checkedTask : ''}`}>
+                    {title}
+                </label>
+            </div>
+            <Image
+                alt='trash-icon'
+                src={TrashIcon}
+                className={styles.buttonTrash}
+                onClick={() => setIsOpen(true)}
+            />
+        </div>
+        <Modal textButton='Deletar' typeModal={2} isOpen={isOpen} onClose={handleCloseModal} onDeleteTask={handleDeleteButtonClick}/>
+    </>
+  )
+}
